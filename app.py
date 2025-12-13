6e7412e063b4b0d14b6d985a02e2be15e0a6ba2c
Srinivas Ganji <53328237+ganjivasu@users.noreply.github.com>
2025-12-13 18:26:21 -0500

    Fix greeting message and change port to 9000

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from DEV environment!!!"
+    return "Hello from DEV environment!"
 
-app.run(host="0.0.0.0", port=8080)
+app.run(host="0.0.0.0", port=9000)
