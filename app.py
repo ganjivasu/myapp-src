21400c9f9cedaf57a47b28df1df13443c3aebb3d
Srinivas Ganji <53328237+ganjivasu@users.noreply.github.com>
2025-12-14 03:38:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello from DEV environment!"
+@app.route("/health")
+def health():
+    return "OK", 200
 
-app.run(host="0.0.0.0", port=9000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=9000)
